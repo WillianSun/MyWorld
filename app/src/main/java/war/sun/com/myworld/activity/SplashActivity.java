e0a04a465601e57a12a186386d8ed3dac722c552
WillianSun <33215674+WillianSun@users.noreply.github.com>
2018-01-16 15:48:51 +0800

    Update SplashActivity.java

--- app/src/main/java/war/sun/com/myworld/activity/SplashActivity.java
+++ app/src/main/java/war/sun/com/myworld/activity/SplashActivity.java
@@ -12,4 +12,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
+    public void theTest(){
+        //更新后的代码
+    }
 }
