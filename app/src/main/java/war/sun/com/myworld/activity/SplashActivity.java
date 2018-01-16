d20b0686f1e6055ef25c5c7db196acf18a4db03e
tiemaocsdn <tiemaocsdn@qq.com>
2018-01-16 11:59:36 +0800

    测试

---
+++ app/src/main/java/war/sun/com/myworld/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package war.sun.com.myworld.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import war.sun.com.myworld.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
